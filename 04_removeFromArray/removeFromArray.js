ccab9fb735b5cbf22da8d2a34e876008d5c058db
MrHyaena <dolezalmartin131@gmail.com>
2024-09-28 09:55:11 +0200

    third excercise second test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,13 @@
 const removeFromArray = function() {
     let arrayTest = arguments[0]
     let word = arguments[1]
-    let index = arrayTest.indexOf(word);
-    arrayTest.splice(index, 1)
+    let argLength = arguments.length - 1
+
+    for (let i = 0; i < argLength; i++) {
+
+        let index = arrayTest.indexOf(arguments[i + 1]);
+        arrayTest.splice(index, 1)
+    }
 
     return arrayTest
 
