24cd63cfa7dd1f88d055788e6ea6a3abf6615951
MrHyaena <dolezalmartin131@gmail.com>
2024-09-28 09:34:10 +0200

    excercise four test one completes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+    let arrayTest = arguments[0]
+    let word = arguments[1]
+    let index = arrayTest.indexOf(word);
+    arrayTest.splice(index, 1)
+
+    return arrayTest
+
 };
 
 // Do not edit below this line
