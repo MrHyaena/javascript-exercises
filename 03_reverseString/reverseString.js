cb59c384d5ea17795440399ab38e807546797cf8
MrHyaena <dolezalmartin131@gmail.com>
2024-09-28 09:07:50 +0200

    Third excercise first test completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word, char) {
+    const splitArray = word.split(""); 
+    const reversedArray = splitArray.toReversed();
 
+    let reversedWord = reversedArray.join("");
+
+    return reversedWord
 };
 
 // Do not edit below this line
